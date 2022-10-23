da81d64ed27db4322ce7ef26f7372fe2399d58e7
dmnot <tomatsuper1701@gmail.com>
2022-10-23 18:34:06 +0500

    First example of type casting

--- src/ru/synergy/Main.java
+++ src/ru/synergy/Main.java
@@ -1,8 +1,23 @@
 package ru.synergy;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        Object o = new Scanner(System.in); // Object Это класс всех классов который может преобразовываться в любой класс
+        //((Scanner) o).nextInt();//Преобразование объекта класса Object в класс Integer
+        Scanner scanner = null; // Инициализация сканера
+        System.out.println(o instanceof Scanner); // Проверка является ли Object сканером
+        if(o instanceof Scanner){scanner = (Scanner) o;} // Проверка на то что Object является сканером и если равно истине
+        //то Object будет равен сканеру
+        if(scanner != null){
+            scanner.nextInt();
+        }
+
+        //((String) o).length(); Выдает ошибку то что Object  является сканером и не может являтся строкой
+        Object o1 = new String();
+        Object o2 = new Integer(1);
     }
 }
